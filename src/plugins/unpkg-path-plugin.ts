9e5d7df99df1aff528791d0bd77aa70abad31455
adriankiezik <kiezikadrian@gmail.com>
2022-02-08 20:32:33 +0100

    Added caching packages + installed localforage package to handle caching. Now instead of fetching packages each time code changed, it's fetching them only on first import in code.

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,10 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localForage from 'localforage';
+
+const fileCache = localForage.createInstance({
+  name: 'file-cache',
+});
 
 // Plugin for esBuild that fetches and loades packages from unpkg.
 
@@ -43,12 +48,27 @@ export const unpkgPathPlugin = () => {
         }
         // If file being loaded is not local,
         // fetch url provided by resolve function.
+
+        // Check if file has been already cached
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+        if (cachedResult) {
+          return cachedResult;
+        }
+
         const { data, request } = await axios.get(args.path);
-        return {
+
+        const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
+
+        // Store response in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
       });
     },
   };
