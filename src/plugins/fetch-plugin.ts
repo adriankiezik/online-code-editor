91dcd3004f3bfb382d81f8d28d846664a8e16699
adriankiezik <kiezikadrian@gmail.com>
2022-02-09 01:41:31 +0100

    Support css in package imports - Unluckily esbuild doesn't support well css,   so I had to make workaround based on injecting   css into js.

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -10,16 +10,51 @@ export const fetchPlugin = (inputCode: string) => {
   return {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          };
-        }
-        // If file being loaded is not local,
-        // fetch url provided by resolve function.
+      // Handle root file
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: 'jsx',
+          contents: inputCode,
+        };
+      });
 
+      // Handle css files (quite workaround)
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        // Check if file has been already cached
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+        if (cachedResult) {
+          return cachedResult;
+        }
+
+        const { data, request } = await axios.get(args.path);
+
+        const escapedData = data
+          .replace(/\n/g, '')
+          .replace(/"/g, '')
+          .replace(/'/g, '');
+
+        const contents = `
+          const style = document.createElement('style');
+          style.innerText = '${escapedData}';
+          document.head.appendChild(style);
+          `;
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents,
+          resolveDir: new URL('./', request.responseURL).pathname,
+        };
+
+        // Store response in cache
+        await fileCache.setItem(args.path, result);
+
+        return result;
+      });
+
+      // Handle js/jsx files
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         // Check if file has been already cached
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
           args.path
